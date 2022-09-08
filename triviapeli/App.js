2b63645a5fe33fb82b126b42b8275c4ffc6fc05c
heuui <henni.haavistola@gmail.com>
2022-09-08 12:21:09 +0300

    Navigaatio

--- triviapeli/App.js
+++ triviapeli/App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from'@react-navigation/native';
+import { createBottomTabNavigator } from'@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
@@ -30,13 +36,36 @@ export default function App() {
   }
 
   return (
+
     <View style={styles.container}>
       <Text>Triviapeli</Text>
       <Button title="Get question" onPress={getQuestion} />
       <Text>{category}</Text>
       <Text>{question}</Text>
       <StatusBar style="auto" />
+
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              
+              if (route.name === 'Home') {
+                iconName = 'md-home';
+              } else if (route.name === 'Rules') {
+                iconName = 'book-sharp';
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}>
+            
+            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Rules" component={RulesScreen} />
+        </Tab.Navigator>
+    </NavigationContainer>
     </View>
+    
   );
 }
 
