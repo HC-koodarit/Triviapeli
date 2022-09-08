b3499de7cebf9d22ab5a1ac69994d5d65c45c90c
Sofia Lumme <sofia.lumme@gmail.com>
2022-09-08 11:46:17 +0300

    lisätty kategoria näkyviin

--- triviapeli/App.js
+++ triviapeli/App.js
@@ -7,27 +7,33 @@ export default function App() {
   // set data to state
   const [data, setData] = useState([]);
   const [question, setQuestion] = useState('');
+  const [category, setCategory] = useState('');
   const [correctAnswer, setCorrectAnswer] = useState('');
   const [incorrectAnswers, setIncorrectAnswers] = useState([]);
+  const [allAnswers, setAllAnswers] = useState([]);
 
 
   // button for getting the question
   const getQuestion = () => {
     fetch("https://opentdb.com/api.php?amount=1&encode=url3986")
-        .then(response => response.json())
-        .then(data => {
-            setQuestion(decodeURIComponent(data.results[0].question));
-            setCorrectAnswer(decodeURIComponent(data.results[0].correct_answer));
-            console.log(question);
-            console.log(correctAnswer);
-        })
-        .catch(err => console.error(err))
-}
+      .then(response => response.json())
+      .then(data => {
+        setQuestion(decodeURIComponent(data.results[0].question));
+        setCategory(decodeURIComponent(data.results[0].category));
+        setCorrectAnswer(decodeURIComponent(data.results[0].correct_answer));
+        setIncorrectAnswers(decodeURIComponent(data.results[0].incorrect_answers));
+        console.log(question);
+        console.log(correctAnswer);
+        console.log(incorrectAnswers);
+      })
+      .catch(err => console.error(err))
+  }
 
   return (
     <View style={styles.container}>
       <Text>Triviapeli</Text>
       <Button title="Get question" onPress={getQuestion} />
+      <Text>{category}</Text>
       <Text>{question}</Text>
       <StatusBar style="auto" />
     </View>
