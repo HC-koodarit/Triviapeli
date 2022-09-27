b33684250baf8284d93462e12cbdedbfcf2c7249
Kristian Riihela <kristian.riihela@gmail.com>
2022-09-27 15:57:27 +0300

    Kuva lisätty main menuun

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 import Styles from './Styles';
 import SelectDropdown from 'react-native-select-dropdown'
 
@@ -29,6 +29,7 @@ export default function HomeScreen( { navigation }) {
                 title="About"
                 onPress={() => navigation.navigate('About')}
             />
+            <Image source={require('../assets/titlescreen.png')} style={{width: 400, height: 400}} />
         </View>
     );
 
