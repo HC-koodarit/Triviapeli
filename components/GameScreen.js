8d712ec20487a4e616728176dbb7f14aee595b09
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-09-27 14:51:35 +0300

    mergen korjaus

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -77,7 +77,7 @@ export default function GameScreen() {
             [
                 {
                 text: "Ok",
-                onPress: (useEffect()),
+                //onPress: (useEffect()),
                 },
             ]
     );
