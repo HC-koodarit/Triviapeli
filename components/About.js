5c1b33971b20e50fecb3220a7638a78fae612025
Kristian Riihela <kristian.riihela@gmail.com>
2022-09-29 13:20:07 +0300

    About-sivua täydennetty

--- components/About.js
+++ components/About.js
@@ -5,8 +5,18 @@ import Styles from './Styles';
 export default function About() {
 
     return (
-        <View style={Styles.container}>
+        <View style={Styles.aboutContainer}>
             <Text style={Styles.header}>About</Text>
+            <Text style={Styles.normalText}>This game is the work of the Ohjelmistoprojekti 2 (Software Project 2) course at Haaga-Helia University of Applied Sciences.</Text>
+            <Text> </Text>
+            <Text style={Styles.normalText}>The team members in alphabetical order are:</Text>
+            <Text style={Styles.normalText}>- Bergman Sebastian</Text>
+            <Text style={Styles.normalText}>- Haavistola Henni</Text>
+            <Text style={Styles.normalText}>- Lumme Sofia</Text>
+            <Text style={Styles.normalText}>- Paltiala Mari</Text>
+            <Text style={Styles.normalText}>- Riihelä Kristian</Text>
+            <Text style={Styles.normalText}>- Ristikari Daniel</Text>
+            <Text> </Text>
             <Text style={Styles.normalText}>This application uses data from The Open Trivia Database API for trivia questions and answers.</Text>
             <Text> </Text>
             <Text style={Styles.normalText}>All data provided by the API is available under the Creative Commons Attribution-ShareAlike 4.0 International License.</Text>
