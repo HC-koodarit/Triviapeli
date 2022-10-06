32f9dfb8f4fac4a00ad3b72860c68e2e14bc4634
heuui <henni.haavistola@gmail.com>
2022-10-06 12:31:23 +0300

    kysymysten määrän valinta ja mergestä jääneet muutokset

--- components/PartyModeScreen.js
+++ components/PartyModeScreen.js
@@ -46,6 +46,7 @@ export default function PartyModeScreen({ route, navigation }) {
             <Text style={Styles.normalText}>Drink: {selectedDrink}</Text>
             <Text style={Styles.normalText}>Category: {selectedCategory}</Text>
             <Text style={Styles.normalText}>Difficulty: {selectedDifficulty}</Text>
+            <Text style={Styles.normalText}>Questins per Player: {selectedNum}</Text>
             <TextInput
                 style={Styles.textInput}
                 placeholder='Amount'
