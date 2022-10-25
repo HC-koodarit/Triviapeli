412a492c6fea01d3c91e7bcb718ff95562d36042
Kristian Riihela <kristian.riihela@gmail.com>
2022-10-25 13:49:00 +0300

    sqlite lisätty

--- components/PartyModeScreen.js
+++ components/PartyModeScreen.js
@@ -6,16 +6,90 @@ import Styles from './Styles.js';
 import PartyModeOptions from './PartyModeOptions.js';
 import { getDatabase, push, ref, onValue, remove } from 'firebase/database';
 import { app } from '../firebase/firebaseconfig.js';
+import * as SQLite from 'expo-sqlite';
 
 export default function PartyModeScreen({ route, navigation, params }) {
 
     // passed params from PartyModeOptions
-    const { selectedCategory, selectedDifficulty, selectedDrink, playerNames} = route.params;
+    const { selectedCategory, selectedDifficulty, selectedDrink, playerNames } = route.params;
     const [activePlayer, setActivePlayer] = useState(0);
 
     const [firebasePoints, setFirebasePoints] = useState([]);
     const [points, setPoints] = useState('');
+
+
+    // ---TÄSTÄ ALKAA SQLITE-HOMMELIT---
+    // make player profile for each player and use sqlite to store the data
+    const db = SQLite.openDatabase('players.db');
+
+    // create table for players
+    useEffect(() => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'create table if not exists players (id integer primary key not null, name text, points int, powerup text, drink text);'
+            );
+        });
+    }, []);
+
+    // insert player profiles into the table
+    const insertPlayer = (name, points, powerup, drink) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'insert into players (name, points, powerup, drink) values (?, ?, ?, ?);',
+                [playerNames[0], 0, 'none', 'none'],
+                null,
+                null
+            );
+        });
+    }
+
+    // update player profiles
+    const updatePlayer = (id, name, points, powerup, drink) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'update players set name = ?, points = ?, powerup = ?, drink = ? where id = ?;',
+                [name, points, powerup, drink, id],
+                null,
+                null
+            );
+        });
+    }
+
+    // delete player profiles
+    const deletePlayer = (id) => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'delete from players where id = ?;',
+                [id],
+                null,
+                null
+            );
+        });
+    }
+
+    // get all player profiles
+    const getPlayers = () => {
+        db.transaction(tx => {
+            tx.executeSql(
+                'select * from players;',
+                [],
+                (_, { rows: { _array } }) => setPlayers(_array),
+                null,
+                null
+            );
+        });
+    }
     
+    // get all player profiles from the table
+    useEffect(() => {
+        getPlayers();
+    }, []);
+
+    // ---TÄHÄN LOPPUU SQLITE-HOMMELIT---
+
+
+
+
     // Initialize Firebase
     const database = getDatabase(app);
 
@@ -23,20 +97,20 @@ export default function PartyModeScreen({ route, navigation, params }) {
     useEffect(() => {
         const itemsRef = ref(database, 'points/');
         onValue(itemsRef, (snapshot) => {
-        const data = snapshot.val();
-        const items = data ? Object.keys(data).map(id => ({ id, ...data[id]})) : [];
-        setFirebasePoints(items);
+            const data = snapshot.val();
+            const items = data ? Object.keys(data).map(id => ({ id, ...data[id] })) : [];
+            setFirebasePoints(items);
         })
     }, []);
 
     // Save item to firebase realtime database
     const saveItem = () => {
         push(
-        ref(database, 'points/'),
-        { 'points': points });
+            ref(database, 'points/'),
+            { 'points': points });
     }
 
-    return(
+    return (
         <View style={Styles.container}>
             <Text style={Styles.normalText}>Player: {activePlayer}</Text>
             <Text style={Styles.normalText}>Drink: {selectedDrink}</Text>
@@ -46,9 +120,9 @@ export default function PartyModeScreen({ route, navigation, params }) {
             <TextInput
                 style={Styles.textInput}
                 placeholder='Amount'
-                onChangeText={points => setPoints(points) }
+                onChangeText={points => setPoints(points)}
             />
-            <Button title="Save" onPress= {saveItem} />
+            <Button title="Save" onPress={saveItem} />
         </View>
     );
 };
