c1327226a4169fb30945b380fbd8bda08bf77326
heuui <henni.haavistola@gmail.com>
2022-11-03 16:13:12 +0200

    clean

--- components/Database.js
+++ components/Database.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { SafeAreaView, Text, View, TextInput, FlatList, ScrollView } from 'react-native';
 import { Button } from 'react-native-elements';
 import Styles from './Styles';
+//import { getDatabase, push, ref, onValue, remove } from 'firebase/database';
+//import { app } from '../firebase/firebaseconfig.js';
 import { MultiSelect, Dropdown } from 'react-native-element-dropdown';
 import * as SQLite from 'expo-sqlite';
 
