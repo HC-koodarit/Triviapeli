437f9bffcdfd58223cd284f8cd78bb7858f6bcb6
Kristian Riihela <kristian.riihela@gmail.com>
2022-11-03 12:32:17 +0200

    uudet buttonit ja ikonit aloitusruutuun

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -10,29 +10,98 @@ export default function HomeScreen({ navigation }) {
     return (
         <SafeAreaView style={Styles.container}>
             <Text style={Styles.header}>Trivia</Text>
+
             <Button
                 title="Partymode"
-                type="outline"
-                size="lg"
-                onPress={() => { navigation.navigate('Partymode')}}
+                icon={{
+                    name: 'users',
+                    type: 'font-awesome',
+                    size: 20,
+                    color: 'white',
+                }}
+                iconContainerStyle={{ marginRight: 10 }}
+                titleStyle={{ fontWeight: '700' }}
+                buttonStyle={{
+                    backgroundColor: 'rgba(80, 154, 230, 1)',
+                    borderColor: 'transparent',
+                    borderWidth: 0,
+                    borderRadius: 30,
+                }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 20,
+                }}
+                onPress={() => { navigation.navigate('Partymode') }}
             />
-            <Text> </Text>
             <Button
                 title="Quickplay"
-                type="outline"
-                onPress={() => navigation.navigate('Quickplay')}
+                icon={{
+                    name: 'user',
+                    type: 'font-awesome',
+                    size: 20,
+                    color: 'white',
+                }}
+                iconContainerStyle={{ marginRight: 10 }}
+                titleStyle={{ fontWeight: '700' }}
+                buttonStyle={{
+                    backgroundColor: 'rgba(80, 154, 230, 1)',
+                    borderColor: 'transparent',
+                    borderWidth: 0,
+                    borderRadius: 30,
+                }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 20,
+                }}
+                onPress={() => { navigation.navigate('Quickplay') }}
             />
-            <Text> </Text>
             <Button
                 title="Rules"
-                type="outline"
-                onPress={() => navigation.navigate('Rules')}
+                icon={{
+                    name: 'comment',
+                    type: 'font-awesome',
+                    size: 20,
+                    color: 'white',
+                }}
+                iconContainerStyle={{ marginRight: 10 }}
+                titleStyle={{ fontWeight: '700' }}
+                buttonStyle={{
+                    backgroundColor: 'rgba(80, 154, 230, 1)',
+                    borderColor: 'transparent',
+                    borderWidth: 0,
+                    borderRadius: 30,
+                }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 20,
+                }}
+                onPress={() => { navigation.navigate('Rules') }}
             />
-            <Text> </Text>
             <Button
                 title="About"
-                type="outline"
-                onPress={() => navigation.navigate('About')}
+                icon={{
+                    name: 'heart',
+                    type: 'font-awesome',
+                    size: 20,
+                    color: 'white',
+                }}
+                iconContainerStyle={{ marginRight: 10 }}
+                titleStyle={{ fontWeight: '700' }}
+                buttonStyle={{
+                    backgroundColor: 'rgba(80, 154, 230, 1)',
+                    borderColor: 'transparent',
+                    borderWidth: 0,
+                    borderRadius: 30,
+                }}
+                containerStyle={{
+                    width: 200,
+                    marginHorizontal: 50,
+                    marginVertical: 20,
+                }}
+                onPress={() => { navigation.navigate('About') }}
             />
 
             <Image source={require('../assets/people.png')} style={
