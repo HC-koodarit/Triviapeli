45826e9896523dd9f6bace139d9bf515a847cfe9
Sofia Lumme <sofia.lumme@gmail.com>
2022-11-17 13:34:22 +0200

    pieni ulkoasumuutos quickplayhin

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -151,7 +151,7 @@ export default function GameScreen({ navigation }) {
         );
     } else {
         return (
-            <View style={Styles.container}>
+            <View style={Styles.PartyModeGameContainer}>
                 <Text style={Styles.normalText}>{message}</Text>
                 <Text style={Styles.normalText}>Points: {points}</Text>
                 <Button title="Next question" onPress={() => getQuestion()} />
