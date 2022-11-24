9dc643577e18706bca889ed76e1edf0a0565eef9
Mardehei <82866317+Mardehei@users.noreply.github.com>
2022-11-24 15:56:10 +0200

    poweruppeja lisätty

--- src/components/PowerUps.js
+++ src/components/PowerUps.js
@@ -1,6 +1,12 @@
 export const PowerUps = [
-    "do a backflip",
+    "take a shot",
     "send a selfie to someone",
     "run around the room",
-    "give a hug to someone"
+    "give a hug to someone",
+    "imitate a chicken",
+    "tell a bad joke",
+    "answer to the next question and drink if the answer is wrong",
+    "kiss on someone on the cheek",
+    "tell a fun detail about themself",
+    "compliment someone",
 ]
